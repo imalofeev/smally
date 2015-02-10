1943198d4cf53a0840aae414eb7464666b78009e
Max <maxovsy@gmail.com>
2015-02-10 22:07:46 +0300

    Update router.php

--- framework/router.php
+++ framework/router.php
@@ -2,7 +2,7 @@
 
 /**
  * Router HTTP-requests to classes. Call when trying to access a non-existent script or
- * if the relative URL begins with a capital letter (mod_rewrite rule)
+ * if the relative URL begins with a capital letter
  * Implemented the following scheme HTTP-requests:
  * (1) /ClassName — invoke static method Index_HTTP or Index (or other, which set in class tag @http_default_static_method)
  *     Parameters are not supported.
